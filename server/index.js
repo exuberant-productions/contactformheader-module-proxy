1263297127b2574bad1dd2d0f7b2c5785b929792
doodoodoom <madridmatt17@gmail.com>
2019-02-26 11:45:46 -0800

    Setup html and server to use dockerized components

--- server/index.js
+++ server/index.js
@@ -1,14 +1,39 @@
 const express = require('express');
 
 const app = express();
-const port = 4000;
+const port = 3000;
 const cors = require('cors');
+
+const proxy = require('http-proxy-middleware');
+
+app.use(express.static(`${__dirname}/../public`));
+
+app.get('/:homeID', (req, res) => {
+  res.redirect(`/index.html?homeId=${req.params.homeID}`);
+});
+
+const options = {
+  target: '/',
+  router: {
+    '/api/homeinfo': 'http://ec2-54-183-109-121.us-west-1.compute.amazonaws.com',
+    '/similarHomes': 'http://ec2-54-215-192-145.us-west-1.compute.amazonaws.com',
+    '/homeDetails': 'http://ec2-54-215-192-145.us-west-1.compute.amazonaws.com',
+    '/homes': 'http://ec2-54-153-58-95.us-west-1.compute.amazonaws.com/',
+    '/home': 'http://ec2-54-153-58-95.us-west-1.compute.amazonaws.com/',
+    '/features': 'http://ec2-54-153-58-95.us-west-1.compute.amazonaws.com/',
+    '/questions': 'http://ec2-54-153-58-95.us-west-1.compute.amazonaws.com/',
+    '/neighborhood': 'http://ec2-54-153-58-95.us-west-1.compute.amazonaws.com/',
+    '/reviews': 'http://ec2-54-153-58-95.us-west-1.compute.amazonaws.com/',
+  }
+};
+
+const proxyServer = proxy(options);
+
+app.use(proxyServer);
 
 app.use(cors());
 
 app.use(express.json());
-
-app.use(express.static(`${__dirname}/../public`));
 
 app.listen(port, () => {
   // console.log(`WE ARE LISTENING ON PORT ${port}`);
